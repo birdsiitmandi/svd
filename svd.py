18ad1192c731867fc74bf3c0cd95e31a58e2a096
birdsiitmandi <36591274+birdsiitmandi@users.noreply.github.com>
2018-03-13 11:26:19 +0530

    Update svd.py
    Updated version of svd.py code. Considers mutual information between frames and also smoothing of data is considered. Thus,better audio segmentation.

--- svd.py
+++ svd.py
@@ -1,3 +1,6 @@
+"""
+@author: rhythm
+"""
 import matplotlib
 import pylab
 import librosa
@@ -11,17 +14,43 @@ import scipy
 import os
 import matplotlib.pyplot as plt 
 import sys
-
-#####default check#####
-if (len(sys.argv)<>3):
-	print("Usage:\n build svd.py pathdir segmentedAudiosDir.\n")
-	sys.exit(-1)
+from math import log
+from scipy import histogram, digitize, stats, mean, std
+from collections import defaultdict
+from sklearn.metrics.cluster import normalized_mutual_info_score
+from numpy import convolve
+####log calculation#####
+log2= lambda x:log(x,2)
 
 ###Find 2^n that is equal to or greater than i.###
 def nextpow2(i):
     n = 1
     while n < i: n *= 2
-    return n
+    return n 
+    
+############probability range##############
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
+    
+    
+############smoothing############## 
+    
+'''def movingaverage (values, window):
+    weights = np.repeat(1.0, window)/window
+    sma = np.convolve(values, weights, 'valid')
+    return sma'''
+def smooth(a,WSZ):
+    # a: NumPy 1-D array containing the data to be smoothed
+    # WSZ: smoothing window size needs, which must be odd number,
+    # as in the original MATLAB implementation
+    out0 = np.convolve(a,np.ones(WSZ,dtype=int),'valid')/WSZ    
+    r = np.arange(1,WSZ-1,2)
+    start = np.cumsum(a[:WSZ-1])[::2]/r
+    stop = (np.cumsum(a[:-WSZ:-1])[::2]/r)[::-1]
+    return np.concatenate((  start , out0, stop  ))
 
 #####variables declaration#####
 #number of training files edit according to number of files you want to use for training
@@ -29,29 +58,32 @@ numTrainFiles=3
 #loop counter
 i=0
 #frame size in seconds
-frameSize = 20e-3
+frameSize = 0.04
 # frame shift in seconds
 frameShift = 10e-3
 #training data path,comment if you want tou use less than 3 files for training and edit the path of your training files. 
-trainPath1="/mnt/6EC3-B6CC/rhythm/Desktop/blackandyellow_grosbeak_GHNP/part_5.wav"
-trainPath2="/home/rhythm/Desktop/seg_task/8.WAV"
-trainPath3="/home/rhythm/Desktop/seg_task/smallhimwoodpeck.wav"
+trainPath1="path to training audio1.wav"
+trainPath2="path to training audio2.wav"
+trainPath3="path to training audio3.wav"
 #test file path
-testPath =sys.argv[1]
+testPath ="path to test audio.wav"
 #svd array initialized to empty
 B=[]
 #threshold percentage to select
-threshProp=1.2
+threshProp=0.7
 #frames above threshold, init to empty
 frames=[]
 #number of frames above threshold
 num=0
 # output dir
-pathdir=sys.argv[2]
+pathdir="/home/rhythm/Desktop/seg_task/testOut"
+#minimum number of frames to be continuous
+numCounters=15
 
 #######training data read and array creation#######
 numColumns=5#number of SVD columns to use
 for c in range(1, numTrainFiles+1):
+    #svd array for each training file initialized to empty
     svdarr=[]
     #var=('trainPath'+str(c)).replace("'", "")
     if c==1:
@@ -70,7 +102,6 @@ for c in range(1, numTrainFiles+1):
     # compute spectrogram
     f, t, U= scipy.signal.spectrogram(x,fs,nfft=nfft1,nperseg=nperseg1,noverlap=noverlap1)
     #print ('traindata shape',U.shape)
-    #D=librosa.power_to_db(U, ref=np.median) # needed??
     x_svd=np.linalg.svd(U)
     W=np.asarray(x_svd[:1])
     W=W.reshape(int((nfft1/2)+1),int((nfft1/2)+1))
@@ -80,58 +111,58 @@ for c in range(1, numTrainFiles+1):
     numC=numColumns*c
     #print c
     #print numC
+    
 B=np.asarray(B)
-#print B.shape
-#transpose of B
-B=B.transpose()
-#print B.shape
 B=B.reshape(numC,int((nfft1/2)+1))
 #print B.shape
+
 #########test data#########
 fs, xtest = scipy.io.wavfile.read(testPath)
 f, t, P= scipy.signal.spectrogram(xtest,fs,nfft=nfft1,nperseg=nperseg1,noverlap=noverlap1) 
 P=np.asarray(P)
 #print ('P shape',P.shape)
 # number of frames in test file
-numTestFrames = (P.shape)[1]
-print ('numtestframes',numTestFrames)
+numFrames = (P.shape)[1]
+print ('numFrames',numFrames)
 
 ################calculate energy#############
-F=np.abs(np.dot(B,P))
-A=[np.sqrt(sum(F[:,i]*F[:,i])) for i in range(numTestFrames)]
+F=np.dot(B,P)
+maF=F.transpose()
+A=[np.sqrt(sum(F[:,i]*F[:,i])) for i in range(numFrames)]
 A=np.asarray(A)
-
-#print("Printing A")
-#print(A[15000])
 #print ('Energy shape',A.shape)
-A = A / np.linalg.norm(A)
-A=np.log(A)
-plt.plot(A)
-plt.show()
-#print A
+A = (A - np.min(A))/(np.max(A)-np.min(A))
 
 #########get frames above threshold###########
-for i in range(numTestFrames):
-    if A[i]>(threshProp*np.mean(A)):
+matEn = np.zeros((numFrames,1))
+matMI = np.zeros((numFrames,1))
+for ix in range(0,numFrames-1):
+    #Mutual Information calculation
+    matMI[ix,0]=normalized_mutual_info_score(maF[ix], softmax(maF[ix]))
+
+for i in range(numFrames):
+    if (matMI[i]>=threshProp):
        num=num+1
        frames.append(i)
-       
+
 #convert into array from list
 frames=np.asarray(frames)
 #print (frames)
 print ('no of frames above threshold are ',num)
 
 ########test sound segmentation##########
-#counters
-x=-1
+x=1
 an=0
+
 #ch_wave and storing
+arr=[]
 for i in range(num):
      counter=0
      selectedFrame=[]     
      if((frames[x]-frames[x-1])==1): #to get frames that are continuous
             while((frames[x]-frames[x-1])==1) and (x<num-2):
                   selectedFrame.append(frames[x])
+                  arr.append(frames[x])
                   counter=counter+1
                   x=x+1
                   #print x
@@ -139,13 +170,15 @@ for i in range(num):
           x=x+1
           #print x       	       
      #print (selectedFrame)
-     if(counter>=10):
-        selectedFrame=np.asarray(selectedFrame)
+     if(counter>=numCounters):
+        yMA = smooth(selectedFrame,1)
+        yMA=np.asarray(yMA)
+        #print yMA
         outpath=pathdir+'/part'+str(an)
         #calculate startTime using an=a+(n-1)d
-        startTime=0+((selectedFrame[0]-1)*frameShift)
+        startTime=0+((yMA[0]-1)*frameShift)
         #calculate endTime using an=a+(n-1)d
-        endTime=frameSize+((selectedFrame[counter-1]-1)*frameShift)
+        endTime=frameSize+((yMA[counter-1]-1)*frameShift)
         #segment and store bird calls
         os.system("ch_wave "+str(testPath)+" -o "+str(outpath)+".wav -start "+str(startTime)+" -end "+str(endTime))
         an=an+1
